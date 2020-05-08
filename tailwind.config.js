06eb0c86f439e517aa17289302b892edae67dab6
Vivian Sarazin <vivian.sarazin.dev@gmail.com>
2020-05-08 12:56:01 +0200

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  purge: [],
+  theme: {
+    container: {
+      padding: "2rem"
+    },
+    colors: {
+      pink: "#f5dcf2",
+      grey: "#e8f4f1",
+      yellow: "#ffcc30",
+      green: "#1ca51d",
+      white: "#ffffff",
+      black: "#000000"
+    }
+  },
+  variants: {},
+  plugins: [],
+}
