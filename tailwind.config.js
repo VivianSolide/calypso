9a4c6a5e168ea0bccd8408985c44061f5d88bb3d
Vivian Sarazin <vivian.sarazin.dev@gmail.com>
2020-05-15 15:25:37 +0200

    harmony4all

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,14 @@ module.exports = {
       green: "#1ca51d",
       white: "#ffffff",
       black: "#000000"
+    },
+    fontSize: {
+      sm: ['15px', '22.5px'],
+      base: ['20px', '30px'],
+      lg: ['25px', '37.5px'],
+      xl: ['30px', '40px'],
+      '2xl': ['45px', '50px'],
+      '3xl': ['65px', '97.5px'],
     }
   },
   variants: {},
